d5736e10dc81c78c935e874027b4f3daafbfc182
Asen Tahchiyski <asen.tahchiyski@puzzel.com>
2021-10-29 13:36:24 +0300

    Working version 1.

---
+++ LineChartUpdatable.js
@@ -0,0 +1,72 @@
+class LineChartUpdatable {
+  x;
+  xAxis;
+  y;
+  yAxis;
+  svg;
+  transitionDuration = 0;
+
+  create = data => {
+    // set the dimensions and margins of the graph
+    const margin = { top: 10, right: 30, bottom: 30, left: 50 },
+      width = 800 - margin.left - margin.right,
+      height = 400 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    this.svg = d3.select("#line-chart")
+      .append("svg")
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+      .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    // Initialise a X axis:
+    this.x = d3.scaleUtc().range([0, width]);
+    this.xAxis = d3.axisBottom().scale(this.x);
+    this.svg.append("g")
+      .attr("transform", `translate(0, ${height})`)
+      .attr("class", "myXaxis")
+
+    // Initialize an Y axis
+    this.y = d3.scaleLinear().range([height, 0]);
+    this.yAxis = d3.axisLeft().scale(this.y);
+    this.svg.append("g")
+      .attr("class", "myYaxis")
+
+    // At the beginning, I run the update function on the first dataset:
+    this.update(data)
+  }
+
+  // Create a function that takes a dataset as input and update the plot:
+  update = data => {
+    // Create the X axis:
+    this.x.domain([d3.min(data, d => d.x), d3.max(data, d => d.x)]);
+    this.svg.selectAll(".myXaxis").transition()
+      .duration(this.transitionDuration)
+      .call(this.xAxis);
+
+    // create the Y axis
+    this.y.domain([d3.min(data, d => d.y), d3.max(data, d => d.y)]);
+    this.svg.selectAll(".myYaxis")
+      .transition()
+      .duration(this.transitionDuration)
+      .call(this.yAxis);
+
+    // Create a update selection: bind to the new data
+    const u = this.svg.selectAll(".lineTest")
+      .data([data], d => d.x);
+
+    // Update the line
+    u
+      .join("path")
+      .attr("class", "lineTest")
+      .transition()
+      .duration(this.transitionDuration)
+      .attr("d", d3.line()
+        .x(d => this.x(d.x))
+        .y(d => this.y(d.y)))
+      .attr("fill", "none")
+      .attr("stroke", "steelblue")
+      .attr("stroke-width", 1)
+  }
+}
---
+++ app.js
@@ -0,0 +1,104 @@
+// Parse data.json
+async function getRawData() {
+  const json = await fetch('./data.json');
+  const parsed = await json.json();
+  return parsed.XXBTZEUR;
+}
+
+// Transform Kraken to D3 data format
+function transformData(item) {
+  return {
+    x: item[2] * 1000,
+    y: parseFloat(item[0])
+  }
+}
+
+// Set up Kraken web socket
+async function startWebSocketConnection(onUpdate) {
+  const socket = new WebSocket('wss://ws.kraken.com');
+
+  socket.onmessage = msg => {
+    const responseData = JSON.parse(msg.data);
+
+    if (responseData.event === 'heartbeat' ||
+      !Array.isArray(responseData[1])) {
+      return;
+    }
+
+    onUpdate(responseData[1]);
+  }
+
+  const subscribe = socket => {
+    const payload = {
+      event: "subscribe",
+      subscription: {
+        name: "trade"
+      },
+      pair: ["XBT/EUR"]
+    };
+
+    socket.send(JSON.stringify(payload));
+  }
+
+  var interval =
+    setInterval(() => {
+      if (socket.readyState) {
+        subscribe(socket);
+        clearInterval(interval);
+      }
+    }, 500);
+
+  window.onbeforeunload = () => {
+    socket.close();
+  }
+}
+
+// Fetch price history
+async function getInitialData() {
+  const response = await fetch('https://api.kraken.com/0/public/Trades?pair=XBTEUR',
+    {
+      mode: 'cors',
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json'
+      }
+    });
+
+  return response.json();
+}
+
+// Update chart with new data
+updateChart = data => {
+  const updateDataSet = [];
+  for (const item of data) {
+    updateDataSet.push(this.transformData(item));
+  }
+
+  this.chartData = [...this.chartData.splice(updateDataSet.length), ...updateDataSet];
+  chart.update(this.chartData);
+}
+
+async function main() {
+  // const chartDataRaw = await getRawData();
+  const chartDataRaw = await getInitialData();
+  this.chartData = chartDataRaw.map(item => this.transformData(item));
+
+  chart.create(chartData);
+
+  await startWebSocketConnection(this.updateChart)
+}
+
+const chart = new LineChartUpdatable();
+let chartData = [];
+main();
+
+// fetch('https://api.kraken.com/0/public/Trades?pair=XBTEUR',
+// {
+//   mode: 'cors',
+//   headers: {
+//     'Access-Control-Allow-Origin': '*',
+//     'Content-Type': 'application/json'
+//   }
+// })
+//   .then(res => res.json())
+//   .then(data => console.log(data));

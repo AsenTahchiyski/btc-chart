ea7f6b0577de53c837c1d9b35f4c883a12114228
Asen Tahchiyski <asen.tahchiyski@puzzel.com>
2021-11-03 14:51:48 +0200

    Some cleanup.

--- app.js
+++ app.js
@@ -14,45 +14,6 @@ function transformDataBinance(item) {
   }
 }
 
-// // Set up Kraken web socket
-// async function startWebSocketConnection(onUpdate) {
-//   const socket = new WebSocket('wss://ws.kraken.com');
-
-//   socket.onmessage = msg => {
-//     console.log('socket msg received');
-//     const responseData = JSON.parse(msg.data);
-
-//     if (responseData.event === 'heartbeat' ||
-//       !Array.isArray(responseData[1])) {
-//       return;
-//     }
-
-//     onUpdate(responseData[1]);
-//   }
-
-//   const subscribe = socket => {
-//     const payload = {
-//       event: "subscribe",
-//       subscription: { name: "trade" },
-//       pair: ["XBT/EUR"]
-//     };
-
-//     socket.send(JSON.stringify(payload));
-//   }
-
-//   var interval =
-//     setInterval(() => {
-//       if (socket.readyState) {
-//         subscribe(socket);
-//         clearInterval(interval);
-//       }
-//     }, 500);
-
-//   window.onbeforeunload = () => {
-//     socket.close();
-//   }
-// }
-
 // Fetch price history
 async function getInitialData() {
   const response = await fetch('https://api.binance.com/api/v3/trades?symbol=BTCEUR')
